d743d4c885e165f8bb3b633138dd3a084095c669
Kyle Chen <kyle.h.chen@gmail.com>
2020-05-06 21:45:11 -0700

    chore: Add mongo configs and graphql packages

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 
