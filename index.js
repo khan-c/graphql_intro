948ce4ca79556061aa6c7315322cfccfac859642
Kyle Chen <kyle.h.chen@gmail.com>
2020-05-11 22:35:57 -0700

    feat: Add grapqhl mutations: newUser and newPost

--- index.js
+++ index.js
@@ -15,30 +15,6 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// );
-
-// const router = express.Router();
-
-// const createNewPost = router.post("/new", (req, res) => {
-//   // remember to import your Post model from Mongoose!
-//   const newPost = new Post({
-//     title: req.body.title,
-//     body: req.body.body,
-//     date: req.body.date,
-//     author: req.body.author,
-//   });
-
-//   newPost
-//     .save()
-//     .then((savedPost) => res.json(savedPost))
-//     .catch((err) => console.log(err));
-// });
-
-// app.use("/posts", createNewPost);
 
 app.use(
   "/graphql",
